350105dcc917711873e36e86ba2e0902f8b1a6b0
Antreas gribas <antreasgribas@gmail.com>
2017-02-23 01:52:15 +0200

    dfs implementation

--- Python/main.py
+++ Python/main.py
@@ -90,12 +90,16 @@ def ChordalGen(n, k):
     """
         Generate a random chordal graph with n vertices, k is the algorithm parameter
     """
+    if 2 * k - 1 > n:
+        raise Exception("ChordalGen parameter k must be lower than n/2")
+
     tree = TreeGen(n)
     # convert to networx before cliquelistgen, that function may alter the
     # children attribute -- not yet
     nx_tree = convert_tree_networkx(tree)
     # nx_export_json(nx_tree)
-
+    print(dfs(tree, tree[0]))
+    print(dfs_list(tree, tree[0]))
     subtrees = [SubTreeGen(tree, k, i) for i in range(0, n)]
 
     # print("subtrees: ", subtrees)
@@ -114,6 +118,8 @@ def ChordalGen(n, k):
     #     print("is Chordal: {0} ".format(nx.is_chordal(nx_g)))
     #     print("is Tree: {0} ".format(nx.is_tree(nx_g)))
     #     print("is Connected: {0} ".format(nx.is_connected(nx_g)))
+    print("is isomophic: {0} ".format(
+        nx.is_isomorphic(nx_chordal, nx_true_chordal)))
     #     print("-------------------")
     nx_export_json([nx_tree, nx_chordal, nx_true_chordal])
 
@@ -194,7 +200,7 @@ def convert_tree_networkx(tree):
     """
         Converts a list of TreeNodes to networkx graph(using children nodes)
     """
-    graph = nx.Graph()
+    graph = nx.Graph(graph_type="tree")
     for treenode in tree:
         for child in treenode.children:
             graph.add_edge(treenode.id, child.id)
@@ -206,7 +212,7 @@ def convert_clique_tree_networkx(clique_tree):
     """
         Converts a clique tree to a networkx graph
     """
-    graph = nx.Graph()
+    graph = nx.Graph(graph_type="fast")
     for node in clique_tree:
         for i in range(len(node.cliqueList)):
             for j in range(i + 1, len(node.cliqueList)):
@@ -219,7 +225,7 @@ def convert_adjacency_list_networkx(adj_list_graph):
     """
         Converts an adjacency list graph to a networkx graph
     """
-    graph = nx.Graph()
+    graph = nx.Graph(graph_type="true")
     for node in adj_list_graph:
         for neighbour in node.Ax:
             graph.add_edge(node.id, neighbour.id)
@@ -264,6 +270,36 @@ def is_subset(list1, list2):
     return True
 
 
+def dfs(graph, root):
+    """
+        Goes through a graph using DFS, using sets not returning in order
+    """
+    visited, stack = set(), [root]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(set(vertex.Ax) - visited)
+    return visited
+
+
+def dfs_list(graph, root):
+    """
+        Goes through a graph using DFS,using lists(should be faster)
+    """
+    for node in graph:
+        node.dfs_visited = False
+
+    visited, stack = [], [root]
+    while stack:
+        vertex = stack.pop()
+        if not vertex.dfs_visited:
+            visited.append(vertex)
+            vertex.dfs_visited = True
+            stack.extend((neighbour for neighbour in vertex.Ax if not neighbour.dfs_visited))
+    return visited
+
+
 def random_element(array, index=0):
     """
         Get a random element, and index from given array starting from index to end
@@ -272,5 +308,5 @@ def random_element(array, index=0):
     return array[i], i
 
 
-ChordalGen(7, 3)
+ChordalGen(50, 14)
 print(".....Done")
