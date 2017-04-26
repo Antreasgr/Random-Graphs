d4b36470f35daf2b0d632353cef89ff7248ec5e6
Antreas gribas <antreasgribas@gmail.com>
2017-04-26 20:59:39 +0300

    refactor markenzon algorithm

--- Python/markenzonAlgorithm.py
+++ Python/markenzonAlgorithm.py
@@ -4,33 +4,62 @@ import random
 from UnionFind import UnionFind
 from nx_converters import *
 
+"""
+    Implements the markenzon algorithm version 2 as described in paper
+    @param l holds the number of maximal cliques generated
+        (l < n, since the graph isconnected).
+    @param m The number of edges
+    @param Q stores the contents of the maximal cliques generated.
+    @param S is an array of integers, such that Si is the cardinality of Qi, 1≤i<n.
+    @param L The list of triples L contains the edges of the clique-tree
+        along with their weights; each edge is given by a pair of integers (i, j),
+        which are the positions of its endpoints in the array Q
+"""
 
-def merge_cliques(num_maximal_cliques, m, L, S, Q, upper_bound, rand):
+class MarkenzonParameters(object):
+    __slots__ = ["num_maximal_cliques", "num_edges", "edges_list", "cardinality_array", "cliques"]
+
+    def __init__(self, l, m, L, S, Q):
+        self.num_maximal_cliques = l
+        self.num_edges = m
+        self.edges_list = L
+        self.cardinality_array = S
+        self.cliques = Q
+
+    def __str__(self):
+        return "#:{}\tedges:{}\tedges_list:{}\tcardinalities:{}\tcliques:{}".format(
+            self.num_maximal_cliques, self.num_edges, self.edges_list, self.cardinality_array, self.cliques)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def merge_cliques(mParameters, upper_bound, rand):
+    """
+        Merge cliques
+    """
     dis_set = UnionFind()
-    [dis_set[i] for i in range(num_maximal_cliques)]
+    [dis_set[i] for i in range(mParameters.num_maximal_cliques)]
 
-    while L and m < upper_bound:
-        (a, b, omega), index = rand.next_element(L)
-        del L[index]
+    while mParameters.edges_list and mParameters.num_edges < upper_bound:
+        (a, b, omega), index = rand.next_element(mParameters.edges_list)
+        del mParameters.edges_list[index]
         i = dis_set[a]
         j = dis_set[b]
-        delta = S[i] - omega
-        Delta = S[j] - omega
-        if m + (delta * Delta) <= upper_bound:
-            dis_set.union(i, j, i)
-            Q[i] = Q[i] + Q[j]
-            S[i] = Delta + delta + omega
-            Q[j] = []
-            S[j] = 0
-            m += Delta * delta
+        delta = mParameters.cardinality_array[i] - omega
+        Delta = mParameters.cardinality_array[j] - omega
+        if mParameters.num_edges + (delta * Delta) <= upper_bound:
+            dis_set.union(i, j)
+            mParameters.cliques[i] = mParameters.cliques[i] + mParameters.cliques[j]
+            mParameters.cardinality_array[i] = Delta + delta + omega
+            mParameters.cliques[j] = []
+            mParameters.cardinality_array[j] = 0
+            mParameters.num_edges += Delta * delta
+    
 
 
 def expand_cliques(n, rand):
-    Q = [[0]]
-    S = [0]
-    L = []
-    m = 0
-    l = 0
+    Q, S, L, m, l = [[0]], [0], [], 0, 0
     for u in range(1, n):
         i = rand.next_random(0, l + 1)
         t = rand.next_random(0, S[i] + 1)
@@ -50,7 +79,7 @@ def expand_cliques(n, rand):
             L.append((i, l, t))
         m += t
 
-    return l, m, L, S, Q
+    return MarkenzonParameters(l, m, L, S, Q)
 
 
 if __name__ == '__main__':
@@ -58,9 +87,19 @@ if __name__ == '__main__':
 
     randomizer = Randomizer(2 * NUM_VERTICES)
     cliques = expand_cliques(NUM_VERTICES, randomizer)
-    merge_cliques(*cliques, 50, randomizer)
-    print(cliques)
+    print("- Expand cliques:")
+    print("\t", cliques)
+    # keep a copy of clique tree edges L because merge_cliques is consuming it
 
-    nx_chordal = convert_markenzon_clique_tree_networkx2(
-        cliques[4], NUM_VERTICES)
+    clique_edges = list(cliques.edges_list)
+
+    merge_cliques(cliques, 500, randomizer)
+    print("- Merge cliques:")
+    print("\t", cliques)
+    # print(clique_edges)
+
+    nx_chordal = convert_markenzon_clique_tree_networkx2(cliques.cliques, NUM_VERTICES)
+    print("- Stats:")
+    print("\t nodes:", len(nx_chordal.nodes()))
+    print("\t edges:", len(nx_chordal.edges()))
     nx_export_json([nx_chordal])
